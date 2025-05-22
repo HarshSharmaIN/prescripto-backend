2a7e2708222a1d3d19c693552f94e16133ef137d
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2025-05-22 19:49:48 +0530

    Updated Code

--- config/gcp.js
+++ config/gcp.js
@@ -1,11 +1,23 @@
-import { Storage } from "@google-cloud/storage";
-import dotenv from "dotenv";
-
+import { Storage } from "@google-cloud/storage"; import dotenv from "dotenv";
 dotenv.config();
 
-const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  keyFilename: process.env.GCP_KEY_FILE,
+const keyFile = {
+  type: "service_account",
+  project_id: process.env.GCP_KEY_PROJECT_ID,
+  private_key_id: process.env.GCP_KEY_PRIVATE_KEY_ID,
+  private_key: process.env.GCP_KEY_PRIVATE_KEY,
+  client_email: process.env.GCP_KEY_CLIENT_EMAIL,
+  client_id: process.env.GCP_KEY_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.GCP_KEY_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com"
+}
+
+const storage = new Storage({ 
+  projectId: process.env.GCP_PROJECT_ID, 
+  credentials: keyFile, 
 });
 
 const bucket = storage.bucket(process.env.GCP_BUCKET_NAME);
